24ecd1a448f31af28f22f241c7169961b98c115f
Destiny653 <fokundem.com@gmail.com>
2024-11-22 02:33:50 +0100

    mordified theme

--- context/ThemeContext.js
+++ context/ThemeContext.js
@@ -1,20 +1,27 @@
 "use client";
 
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 
 export const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
-    const [theme, setTheme] = useState("light");
+    const [theme, setTheme] = useState("light"); 
 
     const toggleTheme = () => {
         setTheme(theme === "light" ? "dark" : "light");
     };
+    useEffect(() => { 
+      let  localTheme = typeof window !== 'undefined' && window.localStorage.getItem('theme')
+        const storedTheme = localTheme;
+        if (storedTheme) {
+            setTheme(storedTheme);
+        }
+    }, [theme])
 
     return (
         <ThemeContext.Provider value={{ theme, toggleTheme }}>
-            <div className={`theme-switcher ${theme }`}>
+            <div className={`theme-switcher ${theme}`}>
                 {children}
             </div>
         </ThemeContext.Provider>
--- src/app/components/Theme/Theme.jsx
+++ src/app/components/Theme/Theme.jsx
@@ -8,26 +8,24 @@ export default function Theme() {
   const { theme, toggleTheme } = useContext(ThemeContext)
   return (
     <ul className='theme-panel cursor-pointer absolute bg-[#313131] text-[#fff] w-[140px] flex flex-col gap-[5px] rounded-[8px]'>
-  {    
-  theme !== 'light'? 
-    <li className=' flex gap-[8px] items-center justify-center' onClick={() => {
-        toggleTheme()
+      <li className=' flex gap-[8px] items-center justify-center' onClick={() => {
+        toggleTheme();
+        typeof window !== 'undefined' && window.localStorage.setItem('theme', 'light');
       }}>
         <IoSunnyOutline className='font-[600]' color='gold' />
         <span className='inline pr-[10px]'>
           Light
         </span>
       </li>
-      :
       <li className='flex gap-[8px] items-center justify-center' onClick={() => {
-        toggleTheme()
+        toggleTheme();
+        typeof window !== 'undefined' && window.localStorage.setItem('theme', 'dark');
       }}>
         <IoMdMoon className='font-[600]' color='gold' />
         <span className='inline pr-[10px]'>
           Dark
         </span>
       </li>
-      }
     </ul>
   )
 }
